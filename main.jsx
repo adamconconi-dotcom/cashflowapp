1b7f1573990537ac4f17665f73c3688ea0def443
adamconconi-dotcom <adam.conconi@gmail.com>
2026-02-23 22:05:35 -0800

    Delete main.jsx

--- main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import CashFlowApp from './CashFlowApp.jsx';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<CashFlowApp />);
